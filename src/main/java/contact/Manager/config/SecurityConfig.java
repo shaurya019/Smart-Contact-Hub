abaa738e345281d2ba96e20af7044fe136e4fb39
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2025-01-02 22:52:56 +0530

    authorizer to request

--- src/main/java/contact/Manager/config/SecurityConfig.java
+++ src/main/java/contact/Manager/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -33,6 +35,23 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
         return daoAuthenticationProvider;
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize -> {
+
+//            means these are public - "/home", "/register", "/services" and else are private
+            // authorize.requestMatchers("/home", "/register", "/services").permitAll();
+
+//            means user se start hona wala sab url private ha
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
     }
 
     @Bean
