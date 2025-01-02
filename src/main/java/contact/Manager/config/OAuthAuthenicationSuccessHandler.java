e858242666668bf928ca753512d9f90504adc481
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2025-01-02 23:58:41 +0530

    Add google and github login email extract code and check in db if exist or not

--- src/main/java/contact/Manager/config/OAuthAuthenicationSuccessHandler.java
+++ src/main/java/contact/Manager/config/OAuthAuthenicationSuccessHandler.java
@@ -1,4 +1,101 @@
 package contact.Manager.config;
 
-public class OAuthAuthenicationSuccessHandler {
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import contact.Manager.entities.Providers;
+import contact.Manager.entities.User;
+import contact.Manager.helpers.AppConstants;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import contact.Manager.repsitories.UserRepo;
+import jakarta.servlet.ServletException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuthAuthenicationSuccessHandler implements AuthenticationSuccessHandler {
+
+    Logger logger = LoggerFactory.getLogger(OAuthAuthenicationSuccessHandler.class);
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public void  onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+
+        var oauth2AuthenicationToken = (OAuth2AuthenticationToken) authentication;
+
+        String authorizedClientRegistrationId = oauth2AuthenicationToken.getAuthorizedClientRegistrationId();
+        logger.info(authorizedClientRegistrationId);
+
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info(key + " : " + value);
+        });
+
+        User user = new User();
+        user.setId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
+        user.setPassword("dummy");
+
+
+        if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
+
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created using google.");
+
+        } else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+
+            String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com";
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
+
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId);
+//            user.setProvider(Providers.GITHUB);
+
+            user.setAbout("This account is created using github");
+
+        }else{
+            logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
+        }
+
+        User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
+
+        if (user2 == null) {
+            userRepo.save(user);
+            System.out.println("user saved:" + user.getEmail());
+        }
+
+        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
+    }
 }
