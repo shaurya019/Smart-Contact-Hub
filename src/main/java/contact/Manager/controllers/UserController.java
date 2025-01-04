e05609d99f82733573b1956f8439575cfef8be01
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2025-01-04 13:31:09 +0530

    managing login username

--- src/main/java/contact/Manager/controllers/UserController.java
+++ src/main/java/contact/Manager/controllers/UserController.java
@@ -1,4 +1,55 @@
 package contact.Manager.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import contact.Manager.entities.User;
+import contact.Manager.helpers.Helper;
+import contact.Manager.services.UserService;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    // user dashbaord page
+
+    @RequestMapping(value = "/dashboard")
+    public String userDashboard() {
+        System.out.println("User dashboard");
+        return "user/dashboard";
+    }
+
+    // user profile page
+
+    @RequestMapping(value = "/profile")
+    public String userProfile(Authentication authentication) {
+        String user = Helper.getEmailOfLoggedInUser(authentication);
+        return "user/profile";
+    }
+
+    // user add contacts page
+
+    // user view contacts
+
+    // user edit contact
+
+    // user delete contact
+
 }
--- src/main/java/contact/Manager/helpers/Helper.java
+++ src/main/java/contact/Manager/helpers/Helper.java
@@ -1,4 +1,25 @@
 package contact.Manager.helpers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
 public class Helper {
+    public static String getEmailOfLoggedInUser(Authentication authentication){
+        if(authentication instanceof OAuth2AuthenticationToken){
+            var aOAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            var clientID = aOAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+            var oauth2User = (OAuth2User) authentication.getPrincipal();
+            String username = "";
+            if(clientID.equalsIgnoreCase("google")){
+                username = oauth2User.getAttribute("email").toString();
+            }else if (clientID.equalsIgnoreCase("github")){
+                username = oauth2User.getAttribute("email") != null ? oauth2User.getAttribute("email").toString()
+                        : oauth2User.getAttribute("login").toString() + "@gmail.com";
+            }
+            return username;
+        }
+        return authentication.getName();
+    }
 }
