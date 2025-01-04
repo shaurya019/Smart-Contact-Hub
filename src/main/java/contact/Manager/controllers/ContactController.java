d521f9df0679e304935e1ee9ac8d62a8dc271f44
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2025-01-04 14:39:57 +0530

    Contact form

--- src/main/java/contact/Manager/controllers/ContactController.java
+++ src/main/java/contact/Manager/controllers/ContactController.java
@@ -1,4 +1,17 @@
 package contact.Manager.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contacts")
 public class ContactController {
+
+    @RequestMapping("/add")
+    // add contact page: handler
+    public String addContactView() {
+        return "user/add_contact";
+    }
+
 }
